0690ec54b0be96207f48605fb3edcc754333eab9
YoussefSewalm <eng.yousefsewalem@gmail.com>
2025-03-27 19:36:58 +0200

    Update AnnotationTransformer.java

--- src/main/java/Listeners/AnnotationTransformer.java
+++ src/main/java/Listeners/AnnotationTransformer.java
@@ -7,7 +7,8 @@ import org.testng.annotations.ITestAnnotation;
 
 public class AnnotationTransformer implements IAnnotationTransformer {
     @Override
-    public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
+    public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) 
+    {
         annotation.setRetryAnalyzer(Retry.class);
     }
 }
