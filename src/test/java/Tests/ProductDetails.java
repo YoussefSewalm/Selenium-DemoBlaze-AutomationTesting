91e8a347ac7886cf6d8c1b2a4a8d7b7b5dee11e1
YoussefSewalm <eng.yousefsewalem@gmail.com>
2025-03-27 19:30:21 +0200

    Update ProductDetails.java

--- src/test/java/Tests/ProductDetails.java
+++ src/test/java/Tests/ProductDetails.java
@@ -17,23 +17,22 @@ public class ProductDetails extends TestBase{
 	@Test(groups= {"Regression"})
 	public void VerifyProductDetails(Method method)
 	{
-    startTest(method.getName(), "Verify Product Details");
-    HomePage.Navigate();
-	String Actual_FirstPhoneName = FirstPhoneNameFromHome.GetText();
-	String Actual_SecondPhoneName = SecondPhoneNameFromHome.GetText();
-	String Actual_FirstPhonePrice = FirstPhonePriceFromHome.GetText();
-	String Actual_SecondPhonePrice = SecondPhonePriceFromHome.GetText();
-	FirstPhone.Navigate();
-	PhoneNameFromProductDetails.assertTextContains(Actual_FirstPhoneName);
-	PhonePriceFromProductDetails.assertTextContains(Actual_FirstPhonePrice);
-    AddToCart.Navigate();
-    Alerts.ConfirmAlert();
-    HomePage.Navigate();
-    SecondPhone.Navigate();
-    PhoneNameFromProductDetails.assertTextContains(Actual_SecondPhoneName);
-    PhonePriceFromProductDetails.assertTextContains(Actual_SecondPhonePrice);
-    AddToCart.Navigate();
-    Alerts.ConfirmAlert();
+                startTest(method.getName(), "Verify Product Details");
+                HomePage.Navigate();
+	        String Actual_FirstPhoneName = FirstPhoneNameFromHome.GetText();
+         	String Actual_SecondPhoneName = SecondPhoneNameFromHome.GetText();
+	        String Actual_FirstPhonePrice = FirstPhonePriceFromHome.GetText();
+         	String Actual_SecondPhonePrice = SecondPhonePriceFromHome.GetText();
+         	FirstPhone.Navigate();
+         	PhoneNameFromProductDetails.assertTextContains(Actual_FirstPhoneName);
+        	PhonePriceFromProductDetails.assertTextContains(Actual_FirstPhonePrice);
+                AddToCart.Navigate();
+                Alerts.ConfirmAlert();
+                HomePage.Navigate();
+                SecondPhone.Navigate();
+                PhoneNameFromProductDetails.assertTextContains(Actual_SecondPhoneName);
+                PhonePriceFromProductDetails.assertTextContains(Actual_SecondPhonePrice);
+                AddToCart.Navigate();
+                Alerts.ConfirmAlert();
 	}
-
 }
